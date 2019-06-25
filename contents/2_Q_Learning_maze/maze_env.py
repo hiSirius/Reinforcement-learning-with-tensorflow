99117bd50e2ec7755c652bba1a243330469d7cfa
hiSirius <hiliangzilong@gmail.com>
2019-06-25 15:42:23 +0900

    Update maze_env.py
    通过数组管理maze的障碍物

--- contents/2_Q_Learning_maze/maze_env.py
+++ contents/2_Q_Learning_maze/maze_env.py
@@ -1,13 +1,10 @@
 """
 Reinforcement learning maze example.
-
 Red rectangle:          explorer.
 Black rectangles:       hells       [reward = -1].
 Yellow bin circle:      paradise    [reward = +1].
 All other states:       ground      [reward = 0].
-
 This script is the environment part of this example. The RL is in RL_brain.py.
-
 View more on my tutorial page: https://morvanzhou.github.io/tutorials/
 """
 
@@ -22,9 +19,24 @@ else:
 
 
 UNIT = 40   # pixels
-MAZE_H = 4  # grid height
-MAZE_W = 4  # grid width
+MAZE_H = 8  # grid height
+MAZE_W = 8  # grid widt
 
+# 1:障碍物， 9:寻找目标
+maze = np.array(
+    [
+        [0,0,0,0,0,0,0,0],
+        [0,1,1,1,0,1,1,0],
+        [0,1,0,0,0,1,0,0],
+        [0,0,0,9,0,1,1,0],
+        [0,1,1,1,0,0,0,0],
+        [0,1,0,0,0,1,0,0],
+        [0,1,0,0,0,1,1,0],
+        [0,0,0,0,0,0,0,0],
+    ]
+)
+
+obstacleList = []
 
 class Maze(tk.Tk, object):
     def __init__(self):
@@ -49,33 +61,25 @@ class Maze(tk.Tk, object):
             self.canvas.create_line(x0, y0, x1, y1)
 
         # create origin
-        origin = np.array([20, 20])
+        #origin = np.array([20, 20])
 
-        # hell
-        hell1_center = origin + np.array([UNIT * 2, UNIT])
-        self.hell1 = self.canvas.create_rectangle(
-            hell1_center[0] - 15, hell1_center[1] - 15,
-            hell1_center[0] + 15, hell1_center[1] + 15,
-            fill='black')
-        # hell
-        hell2_center = origin + np.array([UNIT, UNIT * 2])
-        self.hell2 = self.canvas.create_rectangle(
-            hell2_center[0] - 15, hell2_center[1] - 15,
-            hell2_center[0] + 15, hell2_center[1] + 15,
-            fill='black')
+        # 根据maze Array 中为1的点进行描绘
+        # 并加入List 里
+        for i in range(len(maze)):
+            for j in range(len(maze[0])):
+                if maze[i][j] == 1:
+                    self.hell = self.canvas.create_rectangle(UNIT*i,UNIT*j,UNIT*(i+1),UNIT*(j+1),fill='black')
+                    obstacleList.append(self.canvas.coords(self.hell))
+                elif maze[i][j] == 9:
+                    #create oval
+                    #oval_center = origin + UNIT * 2
+                    self.oval = self.canvas.create_oval(
+                    UNIT*6, UNIT*2,
+                    UNIT*7, UNIT*3,
+                    fill='yellow')
 
-        # create oval
-        oval_center = origin + UNIT * 2
-        self.oval = self.canvas.create_oval(
-            oval_center[0] - 15, oval_center[1] - 15,
-            oval_center[0] + 15, oval_center[1] + 15,
-            fill='yellow')
-
-        # create red rect
-        self.rect = self.canvas.create_rectangle(
-            origin[0] - 15, origin[1] - 15,
-            origin[0] + 15, origin[1] + 15,
-            fill='red')
+        #create red rect
+        self.rect = self.canvas.create_rectangle(0, 0,UNIT, UNIT,fill='red')
 
         # pack all
         self.canvas.pack()
@@ -84,10 +88,10 @@ class Maze(tk.Tk, object):
         self.update()
         time.sleep(0.5)
         self.canvas.delete(self.rect)
-        origin = np.array([20, 20])
+        #origin = np.array([20, 20])
         self.rect = self.canvas.create_rectangle(
-            origin[0] - 15, origin[1] - 15,
-            origin[0] + 15, origin[1] + 15,
+            0, 0,
+           UNIT, UNIT,
             fill='red')
         # return observation
         return self.canvas.coords(self.rect)
@@ -96,31 +100,36 @@ class Maze(tk.Tk, object):
         s = self.canvas.coords(self.rect)
         base_action = np.array([0, 0])
         if action == 0:   # up
-            if s[1] > UNIT:
+            if s[1] > UNIT and maze[round(s[2]/UNIT) -1 ,  round((s[3]-UNIT)/UNIT) -1  ] !=1: #没有到边界，且路可走
                 base_action[1] -= UNIT
         elif action == 1:   # down
-            if s[1] < (MAZE_H - 1) * UNIT:
+            if s[1] < (MAZE_H - 1) * UNIT and  maze[round(s[2]/UNIT) - 1, round((s[3]+UNIT)/UNIT) -1] !=1: #没有到边界，且路可走
                 base_action[1] += UNIT
         elif action == 2:   # right
-            if s[0] < (MAZE_W - 1) * UNIT:
+            if s[0] < (MAZE_W - 1) * UNIT and  maze[round((s[2]+UNIT)/UNIT) -1, round(s[3]/UNIT) - 1] !=1: #没有到边界，且路可走
                 base_action[0] += UNIT
         elif action == 3:   # left
-            if s[0] > UNIT:
+            if s[0] > UNIT and  maze[round((s[2]-UNIT)/UNIT) -1, round(s[3]/UNIT) -1] !=1: #没有到边界，且路可走
                 base_action[0] -= UNIT
 
         self.canvas.move(self.rect, base_action[0], base_action[1])  # move agent
 
         s_ = self.canvas.coords(self.rect)  # next state
 
+        #  下一步是不是碰墙
+        if obstacleList.count(s_) > 0 :
+            return s, 0 ,False
+
         # reward function
         if s_ == self.canvas.coords(self.oval):
             reward = 1
             done = True
             s_ = 'terminal'
-        elif s_ in [self.canvas.coords(self.hell1), self.canvas.coords(self.hell2)]:
-            reward = -1
-            done = True
-            s_ = 'terminal'
+        # 如果碰壁了
+        #elif obstacleList.count(s_) > 0 :
+        #    reward = -1
+        #    done = True
+        #    s_ = 'terminal'
         else:
             reward = 0
             done = False
